f03ce1756fda760308dbfe6baeb299062bba936d
Alex Denbaugh <alex.denbaugh@gmail.com>
2021-02-26 10:52:20 -0800

    Saving work, not yet completed.

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,13 @@
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(addTwoNumbers(4, 4));
+
+function convertHoursToMinutes(number) {
+  var minutes = number * 60;
+  return minutes;
+}
+
+var minutes = convertHoursToMinutes(2);
+console.log('Minutes:', minutes);
